6f60aa97dd2c7fd267cdbce9dea3e6c0f87aed7f
Your Name <you@example.com>
2022-11-09 14:23:09 +0200

    added code files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /application 
+
+COPY x.java .
+
+RUN javac x.java
+
+CMD java x
---
+++ raneem.java
@@ -0,0 +1,7 @@
+public class raneem
+{
+    public static void main(String args[])
+    {
+        System.out.println("raneem");
+    }
+}
